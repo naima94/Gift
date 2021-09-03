1b40d8cace3f5dc74de7803424cbde4390999710
naima94 <naimatou.adambi@gmail.com>
2021-09-03 15:57:34 -0800

    Header scroll end

--- monsript.js
+++ monsript.js
@@ -45,3 +45,11 @@ function scrollActive() {
     })
 }
 window.addEventListener("scroll", scrollActive)
+
+/* ================ CHANGE BACKGROUND HEADER ================ */
+function scrollHeader(){
+    const header = document.getElementById("header")
+    //When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 200) header.classList.add("scroll-header"); else header.classList.remove("scroll-header")
+}
+window.addEventListener("scroll", scrollHeader)
